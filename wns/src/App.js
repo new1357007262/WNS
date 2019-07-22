82f060713a95346c3ed4933df03463b877ad72e6
new1357007262 <1357007262@qq.com>
2019-07-22 11:31:03 +0800

    ''

--- wns/src/App.js
+++ wns/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
- 
+import {Route,Switch,BrowserRouter} from 'react-router-dom' 
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route exact path='/' component={}></Route>
+        
+      </BrowserRouter>
     </div>
   );
 }
